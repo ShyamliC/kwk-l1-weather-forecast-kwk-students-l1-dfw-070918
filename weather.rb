80cb8c94a143f8ea02b6c72657daba7a61fe594e
ShyamliC <shyamli.channa@gmail.com>
2018-07-12 13:58:38 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -8,7 +8,6 @@
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
 def location(weather)
-  
 	
 end
 
